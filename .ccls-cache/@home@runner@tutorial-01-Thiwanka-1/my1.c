cce8a65c9f6bc87e1a5e7ab69a60177f41985b10
Thiwanka-1 <128372987+Thiwanka-1@users.noreply.github.com>
2023-03-21 17:46:00 +0000

    Completed the Tutorial 01

--- .ccls-cache/@home@runner@tutorial-01-Thiwanka-1/my1.c
+++ .ccls-cache/@home@runner@tutorial-01-Thiwanka-1/my1.c
@@ -1,7 +1,3 @@
-/* Exercise 1 - Calculations
-
-   Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
-
 #include <stdio.h>
 
 int main() {
@@ -21,4 +17,3 @@ int main() {
   
   return 0;
 }
-
